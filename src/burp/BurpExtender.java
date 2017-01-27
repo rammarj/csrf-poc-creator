ebce5a4887c59807e5a4b8394a99b58d6fc467d7
JoaquinRMtz <joaquin.ramirez.mtz.lab@gmail.com>
2017-01-27 16:46:25 -0600

    Some insignificant changes

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -28,51 +28,51 @@ import javax.swing.JTabbedPane;
  */
 public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionListener {
 
-    private IBurpExtenderCallbacks iex_callbacks;
-    private IExtensionHelpers iex_helpers;
-    private PocTabManager poc_tab_manager;
-    private IContextMenuInvocation ic_menu_inv;
-    private final JMenuItem send_menu_item;
-    private int tab_count;
-    private LinkedList<JMenuItem> menu_items;
+    private IBurpExtenderCallbacks iexCallbacks;
+    private IExtensionHelpers iexHelpers;
+    private PocTabManager pocTabManager;
+    private IContextMenuInvocation icMenuInvocation;
+    private final JMenuItem sendMenuItem;
+    private int tabCount;
+    private LinkedList<JMenuItem> menuItems;
     /**Initialize all variables needed*/
     public BurpExtender() {
-        this.menu_items = new LinkedList<>();
-        this.send_menu_item = new JMenuItem("send to CSRF PoC Creator");
-        this.send_menu_item.addActionListener(this);
-        menu_items.add(this.send_menu_item);
-        this.tab_count = 1;
+        this.menuItems = new LinkedList<>();
+        this.sendMenuItem = new JMenuItem("send to CSRF PoC Creator");
+        this.sendMenuItem.addActionListener(this);
+        menuItems.add(this.sendMenuItem);
+        this.tabCount = 1;
     }
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks ibec) {
-        this.iex_callbacks = ibec;
-        this.iex_helpers = ibec.getHelpers();
-        this.poc_tab_manager = new PocTabManager();
+        this.iexCallbacks = ibec;
+        this.iexHelpers = ibec.getHelpers();
+        this.pocTabManager = new PocTabManager();
         ibec.registerContextMenuFactory(this);
         ibec.setExtensionName("CSRF PoC Creator");
-        this.iex_callbacks.addSuiteTab(new Tab("CSRF PoC", this.poc_tab_manager));
+        this.iexCallbacks.addSuiteTab(new Tab("CSRF PoC", this.pocTabManager));
     }
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation icmi) {
-        this.ic_menu_inv = icmi;
+        this.icMenuInvocation = icmi;
         byte invocation_context = icmi.getInvocationContext();
         if (invocation_context == IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST
                 || invocation_context == IContextMenuInvocation.CONTEXT_PROXY_HISTORY
                 || invocation_context == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST) {
-            return menu_items;
+            return menuItems;
         }
         return null;
     }
     /**This method is executed when the "send to csrf ..." was clicked*/
     @Override
     public void actionPerformed(ActionEvent e) {
-        IHttpRequestResponse[] selectedMessages = this.ic_menu_inv.getSelectedMessages();
+        IHttpRequestResponse[] selectedMessages = this.icMenuInvocation.getSelectedMessages();
         for (IHttpRequestResponse ihrr : selectedMessages) {
             try {
                 AjaxPoc PoC = new AjaxPoc(ihrr);
-                this.poc_tab_manager.addTab(String.valueOf((this.tab_count++)), ihrr, PoC.getPoc());
+                this.pocTabManager.addTab(String.valueOf((this.tabCount++)), ihrr, PoC.getPoc());
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(this.poc_tab_manager, ex.getMessage());
+                JOptionPane.showMessageDialog(this.pocTabManager, ex.getMessage());
             }
         }
     }
@@ -80,26 +80,26 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
      * Creates the CSRF POC CREATOR tab
      */
     private class PocTabManager extends JTabbedPane {
-        /**Ads a new tab within this tab with all info about the poc
+        /**Ads pocString new tab within this tab with all requestInfo about the poc
          */
         public void addTab(String title, IHttpRequestResponse request, byte[] poc) {
             PocCreatorTab pocCreatorTab = new PocCreatorTab(request, poc);
             addTab(title, pocCreatorTab);
             int index = getTabCount() - 1;
-            JPanel jPanel = new JPanel();
-            jPanel.setOpaque(false);
-            jPanel.add(new JLabel(getTitleAt(index), getIconAt(index), JLabel.LEFT));
+            JPanel tabContainer = new JPanel();
+            tabContainer.setOpaque(false);
+            tabContainer.add(new JLabel(getTitleAt(index), getIconAt(index), JLabel.LEFT));
             CloseIcon closeIcon = new CloseIcon();
-            JButton jButton = new JButton(closeIcon);
-            jButton.setPreferredSize(new Dimension(closeIcon.getIconWidth(), closeIcon.getIconHeight()));
-            jButton.addActionListener((ActionEvent e) -> {
+            JButton closeTabButton = new JButton(closeIcon);
+            closeTabButton.setPreferredSize(new Dimension(closeIcon.getIconWidth(), closeIcon.getIconHeight()));
+            closeTabButton.addActionListener((ActionEvent e) -> {
                 int indexOfTab = indexOfTab(title); //tabs title does not change
                 if (indexOfTab != -1) {
                     removeTabAt(indexOfTab);
                 }
             });
-            jPanel.add(jButton);
-            setTabComponentAt(index, jPanel);
+            tabContainer.add(closeTabButton);
+            setTabComponentAt(index, tabContainer);
         }
     }
     /**
@@ -107,38 +107,38 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
      */
     private class PocCreatorTab extends JPanel implements ActionListener {
 
-        private ITextEditor text_editor;
-        private IMessageEditor message_editor;
-        private JButton btn_save, btn_copy;//, btn_close;
-        private JFileChooser save_file_dialog;
+        private ITextEditor textEditor;
+        private IMessageEditor messageEditor;
+        private JButton saveButton, copyButton;//, btn_close;
+        private JFileChooser saveFileDialog;
 
         /**
-         * Creates a new tab for a poc
+         * Creates pocString new tab for pocString poc
          *
          * @param req the request to show on the left
          * @param poc the poc code
          */
         PocCreatorTab(IHttpRequestResponse req, byte[] poc) {
             super(new BorderLayout(10, 10));
-            this.btn_save = new JButton("save to file");
-            this.btn_copy = new JButton("copy");
-            this.btn_save.setForeground(Color.blue);
-            this.btn_copy.addActionListener(this);
-            this.btn_save.addActionListener(this);
-            this.save_file_dialog = new JFileChooser();
-            /*Create a TextEditor*/
-            this.text_editor = iex_callbacks.createTextEditor();
+            this.saveButton = new JButton("save to file");
+            this.copyButton = new JButton("copy");
+            this.saveButton.setForeground(Color.blue);
+            this.copyButton.addActionListener(this);
+            this.saveButton.addActionListener(this);
+            this.saveFileDialog = new JFileChooser();
+            /*Create pocString TextEditor*/
+            this.textEditor = iexCallbacks.createTextEditor();
             /*Making our message editor great with burp normal popup menu*/
-            this.message_editor = iex_callbacks.createMessageEditor(new IMessageEditorController() {
+            this.messageEditor = iexCallbacks.createMessageEditor(new IMessageEditorController() {
                 @Override
                 public IHttpService getHttpService() {
-                    IRequestInfo analyzeRequest = iex_helpers.analyzeRequest(req);
+                    IRequestInfo analyzeRequest = iexHelpers.analyzeRequest(req);
                     return new IHttpServiceImpl(analyzeRequest);
                 }
 
                 @Override
                 public byte[] getRequest() {
-                    return message_editor.getMessage();
+                    return messageEditor.getMessage();
                 }
 
                 @Override
@@ -146,29 +146,29 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
                     return req.getResponse();
                 }
             }, true);
-            JSplitPane jSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-            jSplitPane.add(this.message_editor.getComponent());
-            jSplitPane.add(this.text_editor.getComponent());
-            add("Center", jSplitPane);
+            JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+            splitPane.add(this.messageEditor.getComponent());
+            splitPane.add(this.textEditor.getComponent());
+            add("Center", splitPane);
             //buttons panel
-            JPanel jPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-            jPanel.add(this.btn_copy);
-            jPanel.add(this.btn_save);
+            JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            buttonsPanel.add(this.copyButton);
+            buttonsPanel.add(this.saveButton);
             //add buttons to end
-            add("South", jPanel);
-            this.text_editor.setText(poc);
-            this.message_editor.setMessage(req.getRequest(), true);
-            iex_callbacks.customizeUiComponent(this);//burp lookandfeel
+            add("South", buttonsPanel);
+            this.textEditor.setText(poc);
+            this.messageEditor.setMessage(req.getRequest(), true);
+            iexCallbacks.customizeUiComponent(this);//burp lookandfeel
         }
 
         /**
-         * When a button is clicked into this tab
+         * When pocString button is clicked into this tab
          */
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == this.btn_copy) {
+            if (e.getSource() == this.copyButton) {
                 copy();
-            } else if (e.getSource() == this.btn_save) {
+            } else if (e.getSource() == this.saveButton) {
                 save();
             }
         }
@@ -177,19 +177,20 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
          * Passes the poc code to the system clipboard
          */
         private void copy() {
-            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(iex_callbacks.getHelpers()
-                    .bytesToString(this.text_editor.getText())), null);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(iexCallbacks.getHelpers()
+                    .bytesToString(this.textEditor.getText())), null);
         }
 
         private void save() {
-            int showSaveDialog = this.save_file_dialog.showSaveDialog(this.text_editor.getComponent());
+            int showSaveDialog = this.saveFileDialog.showSaveDialog(this.textEditor.getComponent());
             if (showSaveDialog == JFileChooser.APPROVE_OPTION) {
                 try {
-                    File ff = this.save_file_dialog.getSelectedFile();
-                    try (FileWriter osw = new FileWriter(ff); BufferedWriter bw = new BufferedWriter(osw)) {
-                        bw.write(iex_callbacks.getHelpers().bytesToString(this.text_editor.getText()));
-                        bw.flush();
-                        osw.flush();
+                    File file = this.saveFileDialog.getSelectedFile();
+                    try (FileWriter fileWriter = new FileWriter(file); 
+                            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                        bufferedWriter.write(iexCallbacks.getHelpers().bytesToString(this.textEditor.getText()));
+                        bufferedWriter.flush();
+                        fileWriter.flush();
                     }
                 } catch (IOException ex) {
                     JOptionPane.showMessageDialog(this, ex, "Error", JOptionPane.ERROR_MESSAGE);
@@ -216,28 +217,28 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
 
         @Override
         public byte[] getPoc() {
-            String sep = System.lineSeparator();
-            StringBuilder a = new StringBuilder();
-            a.append("<html>").append(sep).append("  <!-- CSRF PoC - generated by Burp Suite i0 SecLab plugin -->").append(sep);
-            a.append("<body>").append(sep).append("    <script>\n      function submitRequest()").append(sep);
-            a.append("      {").append(sep).append("        var xhr = new XMLHttpRequest();").append(sep);
+            String lineSeparator = System.lineSeparator();
+            StringBuilder pocString = new StringBuilder();
+            pocString.append("<html>").append(lineSeparator).append("  <!-- CSRF PoC - generated by Burp Suite i0 SecLab plugin -->").append(lineSeparator);
+            pocString.append("<body>").append(lineSeparator).append("    <script>\n      function submitRequest()").append(lineSeparator);
+            pocString.append("      {").append(lineSeparator).append("        var xhr = new XMLHttpRequest();").append(lineSeparator);
             String method;
-            IRequestInfo info = iex_helpers.analyzeRequest(this.request);
-            method = info.getMethod();
-            a.append("        xhr.open(\"").append(method).append("\", \"");
+            IRequestInfo requestInfo = iexHelpers.analyzeRequest(this.request);
+            method = requestInfo.getMethod();
+            pocString.append("        xhr.open(\"").append(method).append("\", \"");
 
             if ("GET".equals(method)) {
-                a.append(this.request.getUrl()).append("\", true);").append(sep);
-                a.append("        xhr.send();\n");
+                pocString.append(this.request.getUrl()).append("\", true);").append(lineSeparator);
+                pocString.append("        xhr.send();\n");
             } else {
-                a.append(this.request.getUrl()).append("\", true);").append(sep);
-                String body = iex_helpers.bytesToString(this.request.getRequest()).substring(info.getBodyOffset());
+                pocString.append(this.request.getUrl()).append("\", true);").append(lineSeparator);
+                String body = iexHelpers.bytesToString(this.request.getRequest()).substring(requestInfo.getBodyOffset());
                 body = Util.escapeBackSlashes(body);
                 body = Util.escapeDoubleQuotes(body);
                 String accept = "xt/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
                 String content = "text/plain";
                 String language = "es-ES,es;q=0.8";
-                for (Parameter next : Util.parseHeaderList(info.getHeaders())) {
+                for (Parameter next : Util.parseHeaderList(requestInfo.getHeaders())) {
                     if ("Accept".equals(next.getName())) {
                         accept = next.getValue();
                     }
@@ -248,33 +249,33 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, ActionL
                         language = next.getValue();
                     }
                 }
-                a.append("        xhr.setRequestHeader(\"Accept\", \"").append(accept).append("\");").append(sep);
-                a.append("        xhr.setRequestHeader(\"Content-Type\", \"").append(content).append("\");").append(sep);
-                a.append("        xhr.setRequestHeader(\"Accept-Language\", \"").append(language).append("\");").append(sep);
-                a.append("        xhr.withCredentials = true;").append(sep).append("        var body = ");
+                pocString.append("        xhr.setRequestHeader(\"Accept\", \"").append(accept).append("\");").append(lineSeparator);
+                pocString.append("        xhr.setRequestHeader(\"Content-Type\", \"").append(content).append("\");").append(lineSeparator);
+                pocString.append("        xhr.setRequestHeader(\"Accept-Language\", \"").append(language).append("\");").append(lineSeparator);
+                pocString.append("        xhr.withCredentials = true;").append(lineSeparator).append("        var body = ");
 
-                if (info.getContentType() == IRequestInfo.CONTENT_TYPE_MULTIPART) {
+                if (requestInfo.getContentType() == IRequestInfo.CONTENT_TYPE_MULTIPART) {
                     String[] lines = body.split("\r\n");
                     for (int i = 0; i < lines.length; i++) {
                         String line = lines[i];
                         if (i == lines.length - 1) {
-                            a.append("\"").append(line).append("\\r\\n\";").append(sep);
+                            pocString.append("\"").append(line).append("\\r\\n\";").append(lineSeparator);
                         } else {
-                            a.append("\"").append(line).append("\\r\\n\" +").append(sep);
+                            pocString.append("\"").append(line).append("\\r\\n\" +").append(lineSeparator);
                         }
                     }
                 } else {
-                    a.append("\"").append(body).append("\";").append(sep);
+                    pocString.append("\"").append(body).append("\";").append(lineSeparator);
                 }
-                a.append("        var aBody = new Uint8Array(body.length);").append(sep);
-                a.append("        for (var i = 0; i < aBody.length; i++)").append(sep);
-                a.append("          aBody[i] = body.charCodeAt(i); ").append(sep);
-                a.append("        xhr.send(new Blob([aBody]));").append(sep);
+                pocString.append("        var aBody = new Uint8Array(body.length);").append(lineSeparator);
+                pocString.append("        for (var i = 0; i < aBody.length; i++)").append(lineSeparator);
+                pocString.append("          aBody[i] = body.charCodeAt(i); ").append(lineSeparator);
+                pocString.append("        xhr.send(new Blob([aBody]));").append(lineSeparator);
             }
-            a.append("      }").append(sep).append("    </script>\n    <form action=\"#\">").append(sep);
-            a.append("      <input type=\"button\" value=\"Submit request\" onclick=\"submitRequest();\" />").append(sep);
-            a.append("    </form>").append(sep).append("  </body>").append(sep).append("</html>");
-            return a.toString().getBytes();
+            pocString.append("      }").append(lineSeparator).append("    </script>\n    <form action=\"#\">").append(lineSeparator);
+            pocString.append("      <input type=\"button\" value=\"Submit request\" onclick=\"submitRequest();\" />").append(lineSeparator);
+            pocString.append("    </form>").append(lineSeparator).append("  </body>").append(lineSeparator).append("</html>");
+            return pocString.toString().getBytes();
         }
 
         @Override
